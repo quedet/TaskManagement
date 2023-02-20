368280c74b6be5e9b10fd93d248b8e734dd67ad4
fidelquedet <fidelquedet10@gmail.com>
2023-02-20 10:19:38 +0000

    Custom Form Widget - Flatpickr

--- server/apps/stimulus_basic/urls.py
+++ server/apps/stimulus_basic/urls.py
@@ -4,5 +4,8 @@ from apps.stimulus_basic import views
 app_name = 'stimulus-basic'
 
 urlpatterns = [
-    path('counter/', views.counter_view, name='counter')
+    path('counter/', views.counter_view, name='counter'),
+    # path('turbo_frame_load/', views.turbo_frame_load_view, name='turbo_frame_load'),
+    path('list/', views.list_view, name='task-list'),
+    path('create/', views.create_view, name='task-create')
 ]
--- server/apps/stimulus_basic/views.py
+++ server/apps/stimulus_basic/views.py
@@ -1,5 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
+
+import http
+
+from apps.tasks.models import Task
+from apps.tasks.forms import TaskForm
 
 # Create your views here.
 def counter_view(request):
     return render(request, 'stimulus_basic/counter.html')
+
+def create_view(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Task created successfully!")
+            return redirect(reverse('stimulus-basic:task-list'))
+        status = http.HTTPStatus.UNPROCESSABLE_ENTITY
+    else:
+        form = TaskForm()
+        status = http.HTTPStatus.OK
+    return render(request, "stimulus_basic/create_page.html", {
+        'form': form
+    }, status=status)
+
+def list_view(request):
+    object_list = Task.objects.order_by('-pk')
+    return render(request, "stimulus_basic/list_page.html", {
+        'object_list': object_list
+    })
+
+def update_view(request, pk):
+    instance = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Task Updated successfully!")
+            return redirect(reverse('stimulus-basic:task-list'))
+        status = http.HTTPStatus.UNPROCESSABLE_ENTITY
+    else:
+        form = TaskForm(instance=instance)
+        status = http.HTTPStatus.OK
+    return render(request, "stimulus_basic/update_page.html", {
+        'form': form
+    }, status=status)
+
+def delete_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        task.delete()
+    return render(request, "stimulus_basic/delete_page.html", {
+
+    })
